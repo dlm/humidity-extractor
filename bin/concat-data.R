d5beb0b5a39443d0b975102f6ed2e3df54fadbb6
David Millman <dave@cs.unc.edu>
2018-05-25 12:40:27 -0600

    Add code to concat data

---
+++ bin/concat-data.R
@@ -0,0 +1,20 @@
+append_file <- function(out_fname, x, first) {
+  x_data <- read.csv(x)
+  write.table(x_data, out_fname, append=!first, col.names=first, row.names=FALSE)
+}
+
+
+aggregate_files <- function(input_dir, output_file) {
+  first <- TRUE
+  x <- lapply(
+    list.files(input_dir, full.names=TRUE, recursive=TRUE),
+    function(fname) {
+      append_file(output_file, fname, first)
+      first <<- FALSE
+    }
+  )
+}
+
+input_dir <- "noaa_data"
+output_file <- "noaa_agg_data.csv"
+aggregate_files(input_dir, output_file)
